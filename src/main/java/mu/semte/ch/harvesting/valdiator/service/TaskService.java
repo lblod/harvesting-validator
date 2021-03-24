0ba0c3756bc17f31cba6c109a44e6d4adbfeb429
Nordine Bittich <nordine@bittich.be>
2021-03-24 18:19:10 +0100

    prepare test release

--- src/main/java/mu/semte/ch/harvesting/valdiator/service/TaskService.java
+++ src/main/java/mu/semte/ch/harvesting/valdiator/service/TaskService.java
@@ -14,6 +14,7 @@ import org.apache.jena.graph.Triple;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.system.RiotLib;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
