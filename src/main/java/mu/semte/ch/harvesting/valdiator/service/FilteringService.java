2e9cd9f2811b5eb8ea7428246abefdad69cdec22
Nordine Bittich <contact@bittich.be>
2025-01-21 10:16:39 +0100

    configurable sleep btw jobs

--- src/main/java/mu/semte/ch/harvesting/valdiator/service/FilteringService.java
+++ src/main/java/mu/semte/ch/harvesting/valdiator/service/FilteringService.java
@@ -17,109 +17,115 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class FilteringService {
 
-        private final ShaclService shaclService;
-        private final TaskService taskService;
-        @Value("${sparql.defaultLimitSize}")
-        private int defaultLimitSize;
+    private final ShaclService shaclService;
+    private final TaskService taskService;
+    @Value("${sparql.defaultLimitSize}")
+    private int defaultLimitSize;
 
-        public FilteringService(ShaclService shaclService, TaskService taskService) {
-                this.shaclService = shaclService;
-                this.taskService = taskService;
+    public FilteringService(ShaclService shaclService, TaskService taskService) {
+        this.shaclService = shaclService;
+        this.taskService = taskService;
+    }
+
+    @Value("${application.sleepBetweenJob}")
+    private int sleepMs;
+
+    private void sleep(int sleepMs) {
+        try {
+            log.info("sleep for {} ms to let virtuoso breathe", sleepMs);
+            Thread.sleep(sleepMs);
+        } catch (Throwable e) {
+            log.error("could not sleep", e);
+        }
+    };
+
+    @SneakyThrows
+    public void runFilterPipeline(Task task) {
+        var inputContainer = taskService.selectInputContainer(task).get(0);
+        log.debug("input container: {}", inputContainer);
+        var countTriples = taskService.countTriplesFromFileInputContainer(
+                inputContainer.getGraphUri());
+        var pagesCount = countTriples > defaultLimitSize ? countTriples / defaultLimitSize : 1;
+
+        var fileContainer = DataContainer.builder().build();
+        var graphContainer = DataContainer.builder().build();
+        var resultContainer = DataContainer.builder().graphUri(graphContainer.getUri()).build();
+
+        for (var i = 0; i <= pagesCount; i++) {
+            var threads = new ArrayList<Thread>();
+            var offset = i * defaultLimitSize;
+            var importedTriples = taskService.fetchTripleFromFileInputContainer(
+                    inputContainer.getGraphUri(), defaultLimitSize, offset);
+            for (var mdb : importedTriples) {
+                threads.add(Thread.startVirtualThread(() -> {
+                    // var report = taskService.fetchValidationGraphByDerivedFrom(
+                    // inputContainer.getValidationGraphUri(),
+                    // mdb.derivedFrom());
+
+                    log.info("generate validation reports...");
+                    var report = shaclService.validate(mdb.model().getGraph());
+                    log.info("triples conforms: {}", report.conforms());
+
+                    var validTriples = writeValidTriples(task, fileContainer, report, mdb);
+
+                    var filteredGraph = validTriples.getKey().getGraphUri();
+
+                    taskService.appendTaskResultFile(
+                            task,
+                            graphContainer.toBuilder().graphUri(filteredGraph).build());
+                    if (!report.conforms()) {
+                        var reportModel = ModelUtils.replaceAnonNodes(report.getModel());
+                        writeReport(task, fileContainer, reportModel, mdb.derivedFrom());
+                        writeErrorTriples(task, fileContainer, mdb.model(),
+                                validTriples.getValue(), mdb.derivedFrom());
+                    }
+                }));
+                sleep(sleepMs);
+            }
+            for (var thread : threads) {
+                thread.join();
+            }
         }
 
-        @SneakyThrows
-        public void runFilterPipeline(Task task) {
-                var inputContainer = taskService.selectInputContainer(task).get(0);
-                log.debug("input container: {}", inputContainer);
-                var countTriples = taskService.countTriplesFromFileInputContainer(
-                                inputContainer.getGraphUri());
-                var pagesCount = countTriples > defaultLimitSize ? countTriples / defaultLimitSize : 1;
+        taskService.appendTaskResultGraph(task, resultContainer);
+    }
 
-                var fileContainer = DataContainer.builder().build();
-                var graphContainer = DataContainer.builder().build();
-                var resultContainer = DataContainer.builder().graphUri(graphContainer.getUri()).build();
+    private void writeErrorTriples(Task task, DataContainer fileContainer,
+            Model importedTriples, Model validTriples,
+            String derivedFrom) {
+        var errorTriples = importedTriples.difference(validTriples);
+        log.debug("Number of errored triples: {}", errorTriples.size());
+        var dataContainer = fileContainer.toBuilder()
+                .graphUri(taskService.writeTtlFile(
+                        task.getGraph(), new ModelByDerived(derivedFrom, errorTriples),
+                        "error-triples.ttl"))
 
-                var sleepMs = 50;
-                for (var i = 0; i <= pagesCount; i++) {
-                        var threads = new ArrayList<Thread>();
-                        var offset = i * defaultLimitSize;
-                        var importedTriples = taskService.fetchTripleFromFileInputContainer(
-                                        inputContainer.getGraphUri(), defaultLimitSize, offset);
-                        for (var mdb : importedTriples) {
-                                threads.add(Thread.startVirtualThread(() -> {
-                                        // var report = taskService.fetchValidationGraphByDerivedFrom(
-                                        // inputContainer.getValidationGraphUri(),
-                                        // mdb.derivedFrom());
+                .build();
+        taskService.appendTaskResultFile(task, dataContainer);
+    }
 
-                                        log.info("generate validation reports...");
-                                        var report = shaclService.validate(mdb.model().getGraph());
-                                        log.info("triples conforms: {}", report.conforms());
+    private void writeReport(Task task, DataContainer fileContainer, Model report,
+            String derivedFrom) {
+        var dataContainer = fileContainer.toBuilder()
+                .graphUri(taskService.writeTtlFile(
+                        task.getGraph(), new ModelByDerived(derivedFrom, report),
+                        "validation-report.ttl"))
 
-                                        var validTriples = writeValidTriples(task, fileContainer, report, mdb);
+                .build();
+        taskService.appendTaskResultFile(task, dataContainer);
+    }
 
-                                        var filteredGraph = validTriples.getKey().getGraphUri();
-
-                                        taskService.appendTaskResultFile(
-                                                        task,
-                                                        graphContainer.toBuilder().graphUri(filteredGraph).build());
-                                        if (!report.conforms()) {
-                                                var reportModel = ModelUtils.replaceAnonNodes(report.getModel());
-                                                writeReport(task, fileContainer, reportModel, mdb.derivedFrom());
-                                                writeErrorTriples(task, fileContainer, mdb.model(),
-                                                                validTriples.getValue(), mdb.derivedFrom());
-                                        }
-                                }));
-                                try {
-                                        log.info("sleep for {} ms to let virtuoso breathe", sleepMs);
-                                        Thread.sleep(sleepMs);
-                                } catch (Throwable e) {
-                                        log.error("could not sleep", e);
-                                }
-                        }
-                        for (var thread : threads) {
-                                thread.join();
-                        }
-                }
-
-                taskService.appendTaskResultGraph(task, resultContainer);
-        }
-
-        private void writeErrorTriples(Task task, DataContainer fileContainer,
-                        Model importedTriples, Model validTriples,
-                        String derivedFrom) {
-                var errorTriples = importedTriples.difference(validTriples);
-                log.debug("Number of errored triples: {}", errorTriples.size());
-                var dataContainer = fileContainer.toBuilder()
-                                .graphUri(taskService.writeTtlFile(
-                                                task.getGraph(), new ModelByDerived(derivedFrom, errorTriples),
-                                                "error-triples.ttl"))
-
-                                .build();
-                taskService.appendTaskResultFile(task, dataContainer);
-        }
-
-        private void writeReport(Task task, DataContainer fileContainer, Model report,
-                        String derivedFrom) {
-                var dataContainer = fileContainer.toBuilder()
-                                .graphUri(taskService.writeTtlFile(
-                                                task.getGraph(), new ModelByDerived(derivedFrom, report),
-                                                "validation-report.ttl"))
-
-                                .build();
-                taskService.appendTaskResultFile(task, dataContainer);
-        }
-
-        private Map.Entry<DataContainer, Model> writeValidTriples(Task task, DataContainer fileContainer,
-                        ValidationReport report, ModelByDerived importedTriples) {
-                log.debug("filter non conform triples...");
-                var validTriples = shaclService.filter(importedTriples.model(), report);
-                var dataContainer = fileContainer.toBuilder()
-                                .graphUri(taskService.writeTtlFile(
-                                                task.getGraph(),
-                                                new ModelByDerived(importedTriples.derivedFrom(), validTriples),
-                                                "valid-triples.ttl"))
-                                .build();
-                taskService.appendTaskResultFile(task, dataContainer);
-                return Map.entry(dataContainer, validTriples);
-        }
+    private Map.Entry<DataContainer, Model> writeValidTriples(Task task, DataContainer fileContainer,
+            ValidationReport report, ModelByDerived importedTriples) {
+        log.debug("filter non conform triples...");
+        var validTriples = shaclService.filter(importedTriples.model(), report);
+        var dataContainer = fileContainer.toBuilder()
+                .graphUri(taskService.writeTtlFile(
+                        task.getGraph(),
+                        new ModelByDerived(importedTriples.derivedFrom(), validTriples),
+                        "valid-triples.ttl"))
+                .build();
+        taskService.appendTaskResultFile(task, dataContainer);
+        return Map.entry(dataContainer, validTriples);
+    }
 }
